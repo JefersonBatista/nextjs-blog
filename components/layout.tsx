0b9849a084ae2b58ddae2e4f4dfc8621aa973be4
JefersonBatista <jeferson.de.oliveira.batista@gmail.com>
2026-07-08 10:57:56 -0300

    fix: site title

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 
 const name = 'Jeferson Batista'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = "Jeferson Batista's Portfolio"
 
 export default function Layout({
   children,
